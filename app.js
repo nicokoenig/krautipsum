10f057cbb257fab00c888337cfbcb3546474d6d0
Nico König <Nico Knig>
2016-07-31 17:50:01 +0200

    changes procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var krautgenerator = require('./src/krautgenerator');
 var app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
