8340bf54b6c0c24bb108fef27259e33d57a22e1e
Markus Eckstein <markus.eckstein@gmx.de>
2016-09-25 18:22:18 +0200

    Removed unnecessary comments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var krautgenerator = require('./src/krautgenerator');
 var app = express();
+var router = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,20 +16,15 @@ app.get('/', function(request, response) {
     });
 });
 
-
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/kraut', function(req, res) {
     krautgenerator.createParagraph(function(error, paragraph) {
         res.json({ kraut: paragraph });
     });
 });
 
-// REGISTER OUR ROUTES -------------------------------
+// REGISTER OUR API ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
-
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
