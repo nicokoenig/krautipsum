c80d478dee0a982bbbda29d23799e1eb685cd454
Steffi Hü <Steffi@Nicos-MacBook-Air.local>
2016-09-27 12:56:52 +0200

    Added more randomness to generated kraut, added some more words, added greetings, made site container min-height 100%

--- src/krautgenerator.js
+++ src/krautgenerator.js
@@ -1,25 +1,68 @@
 var krautreader = require('./krautreader');
-var articles = krautreader.getMeSomeKraut('./src/kraut/artikel.txt');
-var nouns = krautreader.getMeSomeKraut('./src/kraut/substantive.txt');
-var verbs = krautreader.getMeSomeKraut('./src/kraut/verben.txt');
-var adjectives = krautreader.getMeSomeKraut('./src/kraut/adjektive.txt');
-var exclamations = krautreader.getMeSomeKraut('./src/kraut/ausrufe.txt');
+var greetingsOrg = krautreader.getMeSomeKraut('./src/kraut/begruessungen.txt');
+var articlesOrg = krautreader.getMeSomeKraut('./src/kraut/artikel.txt');
+var nounsOrg = krautreader.getMeSomeKraut('./src/kraut/substantive.txt');
+var verbsOrg = krautreader.getMeSomeKraut('./src/kraut/verben.txt');
+var adjectivesOrg = krautreader.getMeSomeKraut('./src/kraut/adjektive.txt');
+var exclamationsOrg = krautreader.getMeSomeKraut('./src/kraut/ausrufe.txt');
 
-// thanks man!
-// http://stackoverflow.com/a/7228322
+var greetings = [];
+var articles = [];
+var nouns = [];
+var verbs = [];
+var adjectives = []
+var exclamations = [];
+
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    
+    return array;
+}
+
+function createKrautCopies() {
+    greetings = greetingsOrg.slice(0);
+    articles = articlesOrg.slice(0);
+    nouns = nounsOrg.slice(0);
+    verbs = verbsOrg.slice(0);
+    adjectives = adjectivesOrg.slice(0);
+    exclamations = exclamationsOrg.slice(0);
+}
+
+function shuffleKraut() {
+    greetings = shuffle(greetings);
+    articles = shuffle(articles);
+    nouns = shuffle(nouns);
+    verbs = shuffle(verbs)
+    adjectives = shuffle(adjectives);
+    exclamations = shuffle(exclamations);
+}
+
 function createParagraph(callback) {
+
+    createKrautCopies();
+    shuffleKraut();
+
     let numberOfSentences = random(10, 15);
     let paragraph = '';
+
+    paragraph += getRandomGreeting() + '! ';
 
     for (let i = 0; i < numberOfSentences; i++) {
         paragraph += createSentence();
     }
 
-    paragraph += exclamations[random(0, exclamations.length - 1)];
+    paragraph += getRandomExclamation();
 
     callback(null, paragraph);
 }
@@ -38,27 +81,35 @@ function createSentence() {
             break;
 
         case 5:
-            sentence = getRandomArticle() + ' ' + getRandomNoun() + ' ' + getRandomVerb() + ' ' + getRandomNoun() + ' ' + getRandomNoun() + '. ';
+            sentence = getRandomNoun() + ' und ' + getRandomNoun() + ' ' + getRandomVerb() + ' ' + getRandomAdjective() + ' ' + getRandomNoun() + '. ';
             break;
 
         case 6:
-            sentence = getRandomNoun() + ' und ' + getRandomNoun() + ' ' + getRandomVerb() + ' ' + getRandomAdjective() + ' ' + getRandomNoun() + '. ';
+            sentence = getRandomArticle() + ' ' + getRandomNoun() + ' ' + getRandomVerb() + ' ' + getRandomArticle().toLowerCase() + ' ' + getRandomAdjective() + ' ' + getRandomNoun() + '. ';
             break;
     }
 
     return sentence;
 }
 
+function getRandomGreeting() {
+    return greetings.pop();
+}
+
+function getRandomExclamation() {
+    return exclamations.pop();
+}
+
 function getRandomNoun() {
-    return nouns[random(0, nouns.length - 1)];
+    return nouns.pop();
 }
 
 function getRandomVerb() {
-    return verbs[random(0, verbs.length - 1)];
+    return verbs.pop();
 }
 
 function getRandomAdjective() {
-    return adjectives[random(0, adjectives.length - 1)];
+    return adjectives.pop();
 }
 
 function getRandomArticle() {
