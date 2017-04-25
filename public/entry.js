9c7587419f0f807ed198afbeb4ff0f67ab4e9788
Nico Koenig <nico@nico.local>
2017-04-25 20:39:30 +0200

    Add new design

--- public/entry.js
+++ public/entry.js
@@ -2,7 +2,6 @@
 require("normalize.css/normalize.css");
 require("./style.scss");
 require("./fontello/css/krautipsum.css");
-require("./tooltip/tooltip.css");
 
 // favicon stuff
 require("./favicon.ico");
@@ -10,18 +9,9 @@ require("./favicon-32x32.png");
 
 // js stuff
 var Clipboard = require("clipboard/dist/clipboard.min.js");
-var Tooltip = require("./tooltip/tooltip.js");
-
-// Tooltip
-var tooltip = new Tooltip('.copy', '', false, 2500);
 
 // Clipboard stuff
 var clipboard = new Clipboard('.copy');
 clipboard.on('success', function(e) {
     e.clearSelection();
-    tooltip.showTooltip('Jawoll, copied to clipboard');
-});
-
-clipboard.on('error', function(e) {
-    tooltip.showTooltip('Nein Nein Nein...\nSomething went wrong, sorry :(');
 });
